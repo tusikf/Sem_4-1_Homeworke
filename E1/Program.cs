496a587c386ac5b1baa87399cb733b0e531e878d
Nataly <tusikf@yandex.ru>
2023-07-03 21:29:21 +0300

    done 1

---
+++ E1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Число {number1} в степени числа {number2} = {getStepen(number1,number2)}");
+
+int getStepen(int chislo, int stepen)
+{
+    int result = 1;
+    for (int i = 1; i <= stepen; i++)
+    {
+        result *= chislo;
+    }
+    return result;
+}
